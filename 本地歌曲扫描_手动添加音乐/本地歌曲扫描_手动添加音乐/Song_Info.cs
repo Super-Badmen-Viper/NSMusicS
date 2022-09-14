b92560ab2b3300b001ea32d7997db4d95f6962d1
夫子墨 <1774148579@qq.com>
2022-09-14 08:11:04 +0800

    提交未补全的方案

---
+++ 本地歌曲扫描_手动添加音乐/本地歌曲扫描_手动添加音乐/Song_Info.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 本地歌曲扫描_手动添加音乐
+{
+    class Song_Info
+    {
+        public string Singer_Name;
+        public string Song_Name;
+        public string Song_Aldum;
+        public string Song_Src;
+        public int Song_No;
+
+        public string Song_Name_Table;
+    }
+}
