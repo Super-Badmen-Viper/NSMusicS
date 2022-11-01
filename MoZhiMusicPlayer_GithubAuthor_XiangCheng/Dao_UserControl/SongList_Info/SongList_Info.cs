facc4bfce7fd962dd84190c983c97fb6399608ac
夫子墨 <1774148579@qq.com>
2022-11-01 14:44:12 +0800

    Update_SongListWeb Load And Save

---
+++ MoZhiMusicPlayer_GithubAuthor_XiangCheng/Dao_UserControl/SongList_Info/SongList_Info.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace MoZhiMusicPlayer_GithubAuthor_XiangCheng.Dao_UserControl.SongList_Info
+{
+    public class SongList_Info
+    {
+        //This SongList's Name
+        string SongListName { get; set; }
+        //This SongList's Like_Nums
+        int This_SongList_Like_Nums { get; set; }
+        //This SongList's Image
+        public ImageBrush SongListShowImage { get; set; }
+        //ListView_Info_DataSource  -> SongList
+        List<ListView_Item_Bing> listView_Item_Bings { get; set; }
+
+        //This SongList's WritterUser_ID 
+        int SongList_WritterUser_ID { get; set; }
+        //This SongList's WritterUser_Name
+        string SongList_WritterUser_Name { get; set; }
+        //This SongList's Updateing Date
+        string Date_Update_Time { get; set; }
+    }
+}
---
+++ MoZhiMusicPlayer_GithubAuthor_XiangCheng/Dao_UserControl/SongList_Info/SongList_Info_WebSave.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoZhiMusicPlayer_GithubAuthor_XiangCheng.Dao_UserControl.SongList_Info
+{
+    internal class SongList_Info_WebSave
+    {
+    }
+}
