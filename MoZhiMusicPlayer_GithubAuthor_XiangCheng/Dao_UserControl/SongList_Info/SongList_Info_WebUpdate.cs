db7939496008b35ba99d5f576315a1c4b3e3f3d1
夫子墨 <1774148579@qq.com>
2022-11-01 17:18:10 +0800

    Update_SongListWeb Load And Save

--- MoZhiMusicPlayer_GithubAuthor_XiangCheng/Dao_UserControl/SongList_Info/SongList_Info_WebUpdate.cs
+++ MoZhiMusicPlayer_GithubAuthor_XiangCheng/Dao_UserControl/SongList_Info/SongList_Info_WebUpdate.cs
@@ -8,6 +8,11 @@ namespace MoZhiMusicPlayer_GithubAuthor_XiangCheng.Dao_UserControl.SongList_Info
 {
     public class SongList_Info_WebUpdate
     {
-
+        /*
+         * 1.连接至服务器
+         * 2.下载or上传资源
+         * 3.断开连接
+         * 
+         */
     }
 }
