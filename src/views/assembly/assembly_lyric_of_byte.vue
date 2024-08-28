0e0f95e716ea9f3876dad3d128532e16624a4a0d
翔 程 <1774148579@qq.com>
2024-08-28 19:54:01 +0800

    实验 歌词逐字效果

---
+++ src/views/assembly/assembly_lyric_of_byte.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+name: "assembly_lyric_of_byte"
+})
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped>
+
+</style>
