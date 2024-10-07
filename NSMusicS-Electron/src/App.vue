14984ec4a01bafffa3e1a1338ca7b152f08862e3
翔 程 <1774148579@qq.com>
2024-10-07 17:29:08 +0800

    update fix version 1.0.4

--- NSMusicS-Electron/src/App.vue
+++ NSMusicS-Electron/src/App.vue
@@ -623,7 +623,7 @@
     );
 
     try {
-      store_app_configs_info.version = '1.0.3';
+      store_app_configs_info.version = '1.0.4';
       console.log('Current Version:', store_app_configs_info.version);
       const xmlUrl = 'https://github.com/Super-Badmen-Viper/NSMusicS/releases/download/NSMusicS-Win-Update/NSMusicS.xml';
       await store_app_configs_logic_update.fetchAndParseXML(xmlUrl);
