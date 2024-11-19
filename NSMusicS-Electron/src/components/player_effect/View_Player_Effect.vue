ba053763b7246731cbadfaad7cca3506c221389d
翔 程 <1774148579@qq.com>
2024-11-19 21:02:46 +0800

    update fix find model of album-media

---
+++ NSMusicS-Electron/src/components/player_effect/View_Player_Effect.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+name: "View_Player_Effect.vue"
+})
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped>
+
+</style>
