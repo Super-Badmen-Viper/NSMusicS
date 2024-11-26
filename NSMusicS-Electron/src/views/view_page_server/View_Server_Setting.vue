48abe6855be4a801cdaf9087d42f270e47f1bdad
翔 程 <1774148579@qq.com>
2024-11-26 08:31:44 +0800

    update server manage page and jellyfin-i18n

---
+++ NSMusicS-Electron/src/views/view_page_server/View_Server_Setting.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+name: "View_Server_Setting.vue"
+})
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped>
+
+</style>
