2015c3edaefc060ce041791e0ba4c0d870172bdf
翔 程 <1774148579@qq.com>
2024-11-27 16:01:05 +0800

    update server-manage setting

---
+++ NSMusicS-Electron/src/views/view_page_server/table/type/group_4_2.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <n-space vertical>
+    <span style="font-weight: 600;font-size: 16px;">
+      {{ $t('TabServer') }} >
+    </span>
+    <n-space vertical>
+      Not open || 未开放
+    </n-space>
+    <br>
+  </n-space>
+</template>
+<style>
+
+</style>
