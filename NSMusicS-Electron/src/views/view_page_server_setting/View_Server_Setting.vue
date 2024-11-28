22eb1b5eb44a12038a519a6c765bc9591441e36d
翔 程 <1774148579@qq.com>
2024-11-28 15:52:47 +0800

    Optimized cross platform compatibility of Electron file system access source code

---
+++ NSMusicS-Electron/src/views/view_page_server_setting/View_Server_Setting.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import View_Server_Setting from './table/Table_Server_Setting.vue'
+import {store_router_data_info} from "@/store/router/store_router_data_info";
+
+onMounted(async () => {
+  store_router_data_info.router_select = 'View_Server_Setting'
+});
+</script>
+<template>
+  <div class="view_show">
+    <View_Server_Setting/>
+  </div>
+</template>
+
+<style>
+.view_show {
+  width: 100vw;
+  height: calc(100vh - 200px);
+}
+</style>
