72d1d324fb6a350adb54c80bba4ab64fe6883605
chengxiang <1774148579@qq.com>
2024-12-19 08:32:45 +0800

    update fork style of apple music

---
+++ NSMusicS-Electron/src/views/view_page_player/table/Table_Album_Model_2_Circle.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+name: "Table_Album_Model_2_Circle"
+})
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped>
+
+</style>
---
+++ NSMusicS-Electron/src/views/view_page_player/table/Table_Album_Model_3_Record.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+name: "Table_Album_Model_3_Record"
+})
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped>
+
+</style>
