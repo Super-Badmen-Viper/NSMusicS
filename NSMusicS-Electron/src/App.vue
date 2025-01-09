246b96ef0af601cf4bcf97d1f4067b473fc647e5
翔 程 <1774148579@qq.com>
2025-01-09 17:07:56 +0800

    update version 1.1.8

--- NSMusicS-Electron/src/App.vue
+++ NSMusicS-Electron/src/App.vue
@@ -676,7 +676,7 @@
     }
 
     try {
-      store_app_configs_info.version = '1.1.7';
+      store_app_configs_info.version = '1.1.8';
       console.log('Current Version:', store_app_configs_info.version);
       const xmlUrl = 'https://github.com/Super-Badmen-Viper/NSMusicS/releases/download/NSMusicS-Win-Update/NSMusicS.xml';
       await store_app_configs_logic_update.fetchAndParseXML(xmlUrl);
--- NSMusicS-Electron/vite.config.ts
+++ NSMusicS-Electron/vite.config.ts
@@ -9,6 +9,7 @@ import {viteElectronBuild} from './plugins/vite.electron.build'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
