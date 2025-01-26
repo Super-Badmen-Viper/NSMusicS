f08079da50f527bbf34550a58afa1f318c4ae759
翔 程 <1774148579@qq.com>
2025-01-26 12:01:08 +0800

    update jellyfin api and Architecture mode adjustment - project structure

--- NSMusicS-Electron/src/data/data_access/servers_configs/subsonic_api/services_normal/user_management/index_service.ts
+++
@@ -1,7 +0,0 @@
-import {Subsonic_Api_Services_Normal} from "../Subsonic_Api_Services_Normal";
-
-export class User_ApiService_of_ND extends Subsonic_Api_Services_Normal {
-    public async getUser(username: string,token: string,salt: string): Promise<any> {
-        return this.sendRequest(username, token, salt, 'getUser');
-    }
-}
