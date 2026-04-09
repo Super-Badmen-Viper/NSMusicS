ff0239dc45a4a36b8268470ec94cbf9abf7f401f
翔 程 <1774148579@qq.com>
2026-04-09 16:37:02 +0800

    docker: default web mirror runtime env vars

--- NSMusicS-Electron/docker-entrypoint.sh
+++ NSMusicS-Electron/docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+: "${NGINX_PORT:=80}"
+: "${BACKEND_SERVICE:=127.0.0.1:3321}"
+
 # 替换 Nginx 配置模板中的变量
 envsubst '${NGINX_PORT} ${BACKEND_SERVICE}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
